e8c70c5df5289e2e40b4669478c5186198522fc7
Max Maahn <mmaahn@meteo.uni-koeln.de>
2012-03-27 11:20:42 -0400

    GPL license added

--- IMProToo.py
+++ IMProToo.py
@@ -3,9 +3,24 @@
 ImProToo
 Innominate MRR Processing Tool
 
-(c)2011,2012 Max Maahn, IGMK (mmaahn@meteo.uni-koeln.de)
+Python toolkit to read and write MRR Data. Raw Data, Average and Instantaneous
+Data are supported.
 
-Python toolkit to read and write MRR Data. Raw Data, Average and Instantaneous Data are supported.
+Copyright (C) 2011,2012 Maximilian Maahn, IGMK (mmaahn@meteo.uni-koeln.de)
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 '''
 
 import numpy as np
